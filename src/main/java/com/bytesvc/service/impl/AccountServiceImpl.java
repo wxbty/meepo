a3a36c9b0b8758d06bd02bab33287eb8dfc02fba
wangxb <wangxb@tuya.com>
2018-06-20 14:35:00 +0800

    add

--- src/main/java/com/bytesvc/service/impl/AccountServiceImpl.java
+++ src/main/java/com/bytesvc/service/impl/AccountServiceImpl.java
@@ -16,9 +16,7 @@ public class AccountServiceImpl implements IAccountService {
 	@Transactional(rollbackFor = ServiceException.class)
 	public void increaseAmount(String acctId, double amount) throws ServiceException {
 		int value = this.jdbcTemplate.update("update tb_account_one set amount = amount + ? where acct_id = ?", amount, acctId);
-		if (value != 1) {
-			throw new ServiceException("ERROR!");
-		}
+
 		this.jdbcTemplate.update("update txc_undo_log set status = status + 1 where id = 1");
 		System.out.printf("exec increase: acct= %s, amount= %7.2f%n", acctId, amount);
 	}
@@ -26,9 +24,7 @@ public class AccountServiceImpl implements IAccountService {
 	@Transactional(rollbackFor = ServiceException.class)
 	public void decreaseAmount(String acctId, double amount) throws ServiceException {
 		int value = this.jdbcTemplate.update("update tb_account_one set amount = amount - ? where acct_id = ?", amount, acctId);
-		if (value != 1) {
-			throw new ServiceException("ERROR!");
-		}
+
 		this.jdbcTemplate.update("update txc_undo_log set status = status + 1 where id = 1");
 		System.out.printf("exec decrease: acct= %s, amount= %7.2f%n", acctId, amount);
 		// throw new ServiceException("rollback");
