7795a6d8cd05393a5f505fd5e58e8c7ae477d62f
wangxb <wangxb@tuya.com>
2018-06-21 10:25:48 +0800

    add

--- src/main/java/com/bytesvc/service/main/ProviderMain.java
+++ src/main/java/com/bytesvc/service/main/ProviderMain.java
@@ -7,6 +7,7 @@ public class ProviderMain {
 	static ClassPathXmlApplicationContext context = null;
 
 	public static void main(String... args) throws Throwable {
+		System.out.println("------begin start provider main----------");
 		context = new ClassPathXmlApplicationContext("application.xml");
 		context.start();
 
